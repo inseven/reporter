124265674f67144efb5b4d4b10f450009cf948f8
Jason Morley <hello@jbmorley.co.uk>
2025-09-16 05:56:34 -1000

    Start setting things up

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,6 +5,13 @@ set -o pipefail
 set -x
 set -u
 
+ROOT_DIRECTORY="$( cd "$( dirname "$( dirname "${BASH_SOURCE[0]}" )" )" &> /dev/null && pwd )"
+SCRIPTS_DIRECTORY="$ROOT_DIRECTORY/scripts"
+
+
+source "$SCRIPTS_DIRECTORY/environment.sh"
+
+
 # Remove the build directory if it exists to force a full rebuild.
 if [ -d .build ] ; then
     rm -rf .build
--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -25,8 +25,8 @@ set -o pipefail
 set -x
 set -u
 
-SCRIPTS_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-ROOT_DIRECTORY="$SCRIPTS_DIRECTORY/.."
+ROOT_DIRECTORY="$( cd "$( dirname "$( dirname "${BASH_SOURCE[0]}" )" )" &> /dev/null && pwd )"
+SCRIPTS_DIRECTORY="$ROOT_DIRECTORY/scripts"
 CHANGES_DIRECTORY="$SCRIPTS_DIRECTORY/changes"
 BUILD_TOOLS_DIRECTORY="$SCRIPTS_DIRECTORY/build-tools"
 
