a88a3afc4d8bba8b575126c46207403146d072cb
Jason Morley <hello@jbmorley.co.uk>
2026-02-24 20:10:58 -1000

    fix: Postfix Ubuntu release versions with the release codename

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -87,7 +87,7 @@ case $DISTRO in
             -t deb \
             -p "$PACKAGE_FILENAME" \
             --name "reporter" \
-            --version $VERSION_NUMBER \
+            --version "${VERSION_NUMBER}~${DISTRIB_CODENAME}1" \
             --architecture "$ARCHITECTURE" \
             --description "$DESCRIPTION" \
             --url "$URL" \
