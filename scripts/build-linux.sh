f63690a9aaf4b13811fc8661089ed556ed406e1e
Jason Morley <hello@jbmorley.co.uk>
2026-02-26 09:19:49 -1000

    fix: Add Debian Trixie builds (#90)

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -79,15 +79,13 @@ case $DISTRO in
     ubuntu|debian)
 
         ARCHITECTURE=`dpkg --print-architecture`
-        source /etc/lsb-release
-        OS_VERSION="$DISTRIB_RELEASE"
         PACKAGE_FILENAME="reporter.deb"
         fpm \
             -s dir \
             -t deb \
             -p "$PACKAGE_FILENAME" \
             --name "reporter" \
-            --version "${VERSION_NUMBER}~${DISTRIB_CODENAME}${BUILD_NUMBER}" \
+            --version "${VERSION_NUMBER}~${VERSION_CODENAME}${BUILD_NUMBER}" \
             --architecture "$ARCHITECTURE" \
             --description "$DESCRIPTION" \
             --url "$URL" \
